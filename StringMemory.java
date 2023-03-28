5d286cbd989665d5594a186697e546dd09c0dbdb
includeAZU <96526574+includeAZU@users.noreply.github.com>
2023-03-28 23:16:55 +0800

    Add files via upload

---
+++ StringMemory.java
@@ -0,0 +1,13 @@
+public class StringMemory
+{
+
+    public static void main(String[] args) {
+       String str1="hello";
+       String str2=new String("hello");
+       System.out.println(str1==str2); 
+        String str3="hello";
+        System.out.println(str1==str3); 
+
+
+    }
+}
